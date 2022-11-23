fd911fe46595e9cce7f3e0965e7601a335e7a3d8
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-11-23 16:51:58 +0100

    add check of other flag for language on card

--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,23 @@
 const title = 'boolflix';
-const langFlag = ['en','it', 'es', 'fr', 'hr', 'de'];
+const langFlag = [
+  'en',
+  'it', 
+  'es', 
+  'fr', 
+  'hr', 
+  'de', 
+  'pt', 
+  'ru', 
+  'ms', 
+  'sv', 
+  'mk', 
+  'ro', 
+  'is', 
+  'no', 
+  'fi', 
+  'ar', 
+  'sh', 
+  'pl', 
+  'tr'
+];
 export {title, langFlag};
